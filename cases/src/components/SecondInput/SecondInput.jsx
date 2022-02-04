7fb2b2ebecc833d26c32c359b1b1c882398a7376
Valynoka <valynoka@yandex.ru>
2022-02-04 19:52:13 +0300

    Add changes in Second_ver
    Add some  consonant Letters

--- cases/src/components/SecondInput/SecondInput.jsx
+++ cases/src/components/SecondInput/SecondInput.jsx
@@ -7,36 +7,48 @@ function SecondInput() {
     const [value, setValue] = useState('Падеж');
 
     let input = [...words];
+    console.log(input)
     let lastLetter = input[input.length-1];
+    console.log(lastLetter)
 
+    const consonantLetters = ["б", "в", "м", "г", "д", "л", "ж", "з", "к", "н", "п", "т", "ф", "ч", "ц", 'щ', 'р', 'х']
 
-    //Родительный падеж
-
+    // Родительный падеж
     if (lastLetter === 'а' && value === 'Родительный падеж') {
         input.pop();
         input.push("ы");
+    } else if (lastLetter === consonantLetters.find((item)=> item === lastLetter) && value === 'Родительный падеж'){
+        input.push(input.splice(input.lastIndexOf(lastLetter)+1, 1, 'a'))
     }
 
     //Дательный падеж
     if (lastLetter === 'а' && value === 'Дательный падеж') {
         input.pop();
         input.push('е');
+    } else if (lastLetter === consonantLetters.find((item)=> item === lastLetter) && value === 'Дательный падеж'){
+        input.push(input.splice(input.lastIndexOf(lastLetter)+1, 1, 'у'))
     }
 
     //Винительный падеж
     if (lastLetter === 'а'&& value === 'Винительный падеж') {
         input.pop();
         input.push('у');
+    } else if (lastLetter === consonantLetters.find((item)=> item === lastLetter) && value === 'Винительный падеж'){
+        input.push(input.splice(input.lastIndexOf(lastLetter)+1, 0, ''))
     }
     //Творительный падеж
     if (lastLetter === 'а'&& value === 'Творительный падеж') {
         input.pop();
         input.push('ой');
+    } else if (lastLetter === consonantLetters.find((item)=> item === lastLetter) && value === 'Творительный падеж'){
+        input.push(input.splice(input.lastIndexOf(lastLetter)+1, 1, 'ом'))
     }
     //Предложный падеж
     if (lastLetter === 'а'&& value === 'Предложный падеж') {
         input.pop();
         input.push('е');
+    } else if (lastLetter === consonantLetters.find((item)=> item === lastLetter) && value === 'Предложный падеж'){
+        input.push(input.splice(input.lastIndexOf(lastLetter)+1, 1, 'у'))
     }
 
     return (
